7c08d5ea3bbcb7a2cde6b41710b4902c4948c0c6
Tim Hörmann <pypi@audivir.de>
2024-11-24 23:02:35 +0100

    fixed Battery CLI

--- Casks/battery-cli.rb
+++ Casks/battery-cli.rb
@@ -3,13 +3,14 @@ cask "battery-cli" do
   desc "CLI for managing battery charging."
   homepage "https://github.com/audivir/battery-cli/"
   sha256 "4b342cbebcc13656749951a37a978b49fe3a1a9b73b3bc8dca655f24bbd1767b"
-  url "https://github.com/audivir/battery-cli/archive/refs/tags/0.9.0.tar.gz"
+  version "0.9.0"
+  url "https://github.com/audivir/battery-cli/archive/refs/tags/#{version}.tar.gz"
 
   depends_on macos: ">= :catalina"
   depends_on arch: :arm64
   depends_on formula: "smc"
 
-  binary "battery.sh", target: "battery"
+  binary "#{staged_path}/battery-cli-#{version}/battery.sh", target: "battery"
 
   postflight do
     curr_user = ENV["USER"]
