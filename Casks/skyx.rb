a22903cd45c2d594b0ee30f115999ee4fdf1cf50
Tim Hörmann <pypi@audivir.de>
2024-11-29 09:22:37 +0100

    set right version of sky x

--- Casks/skyx.rb
+++ Casks/skyx.rb
@@ -1,5 +1,5 @@
  cask "skyx" do
-    version "24.1.3-2"
+    version "24.1.3-3"
     sha256 "dce31b764dc81916d303b674bc6ebef4fb8603636c0cc6f74f5c49a7d554f150"
 
     url "https://desktopclient.ott.sky.com/skygodesktop/AT/SkyXInstaller.zip"
