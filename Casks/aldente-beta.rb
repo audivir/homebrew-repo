67dabb175c4e5d6c17c4c4d098262c2252b4644c
Tim Hörmann <pypi@audivir.de>
2025-06-25 12:06:47 +0200

    added aldente-beta 1.33.2

---
+++ Casks/aldente-beta.rb
@@ -0,0 +1,34 @@
+cask "aldente-beta" do
+  version "1.33.2"
+  sha256 "f17b5a8649efe698a122b546f255c5644e486a08eedff77478ab7d1fc611e75c"
+
+  url "https://github.com/AppHouseKitchen/AlDente-Charge-Limiter/releases/download/#{version}/AlDente.dmg"
+  name "AlDente"
+  desc "Menu bar tool to limit maximum charging percentage"
+  homepage "https://apphousekitchen.com/"
+
+  livecheck do
+    url "https://apphousekitchen.com/aldente/aldenteproappcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "AlDente.app"
+
+  uninstall launchctl:  "com.apphousekitchen.aldente-pro.helper",
+            quit:       "com.apphousekitchen.aldente-pro",
+            login_item: "AlDente",
+            delete:     "/Library/PrivilegedHelperTools/com.apphousekitchen.aldente-pro.helper"
+
+  zap trash: [
+    "~/Library/Application Support/AlDente",
+    "~/Library/Caches/com.apphousekitchen.aldente-pro",
+    "~/Library/HTTPStorages/com.apphousekitchen.aldente-pro",
+    "~/Library/HTTPStorages/com.apphousekitchen.aldente-pro.binarycookies",
+    "~/Library/Preferences/com.apphousekitchen.aldente-pro.plist",
+    "~/Library/Preferences/com.apphousekitchen.aldente-pro_backup.plist",
+    "~/Library/Preferences/com.apphousekitchen.aldente-pro_stats.sqlite3",
+  ]
+end
