a757b8881cb7bce9785c2969f74e00267d1a12f4
Tim Hörmann <pypi@audivir.de>
2026-02-16 21:25:25 +0100

    changed rust version for dfhash

--- Formula/dfhash.rb
+++ Formula/dfhash.rb
@@ -6,13 +6,20 @@ class Dfhash < Formula
       revision: "db4956982d21b9b0ce71c211f7e575d5b79e0be4"
   license "MIT"
 
-  depends_on "rust" => :build
+  depends_on "rustup" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    nightly_version = "nightly-2026-01-28"
+
+    ENV["RUSTUP_HOME"] = buildpath/".rustup"
+    ENV["CARGO_HOME"] = buildpath/".cargo"
+    
+    system "rustup", "toolchain", "install", nightly_version
+    
+    system "#{buildpath}/.cargo/bin/cargo", "+#{nightly_version}", "install", *std_cargo_args
   end
 
   test do
-    assert_match "kv #{version}", shell_output("#{bin}/kv --version")
+    assert_match "dfhash #{version}", shell_output("#{bin}/dfhash --version")
   end
 end
