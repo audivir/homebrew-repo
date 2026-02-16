674c4b5590ae726f86081fff9a607dda6f76a58f
Tim Hörmann <pypi@audivir.de>
2026-02-16 19:20:53 +0100

    added fuse-t cask dep

--- Formula/bindfs-fuse-t.rb
+++ Formula/bindfs-fuse-t.rb
@@ -11,13 +11,13 @@ class BindfsFuseT < Formula
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
 
-  on_macos do
-    depends_on "fuse-t"
-  end
-
   conflicts_with "bindfs", because: "it installs the same binaries"
 
   def install
+    # check if fuse-t is installed else die
+    if ! File.exist?("/usr/local/lib/libfuse-t.dylib")
+      odie "fuse-t is not installed. Please install it first: brew install --cask fuse-t"
+    end
     ENV.append_path "HOMEBREW_LIBRARY_PATHS", "/usr/local/lib"
     ENV.append_path "HOMEBREW_INCLUDE_PATHS", "/usr/local/include"
     ENV.append_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
