f476c8c844e33b77fc20b5196ea70196b53ae0eb
Tim Hörmann <pypi@audivir.de>
2026-02-16 19:12:18 +0100

    linted bindfs

--- Formula/bindfs-fuse-t.rb
+++ Formula/bindfs-fuse-t.rb
@@ -1,31 +1,20 @@
 class BindfsFuseT < Formula
   desc "Mount a directory to another location with altered permissions"
   homepage "https://bindfs.org/"
-  # Change URL to point to GitHub and specify the tag
   url "https://github.com/mpartel/bindfs.git",
-      tag: "1.18.3",
+      tag:      "1.18.3",
       revision: "91fbfa48427c67f59c24e84f3855507531ba972c"
   license "GPL-2.0-or-later"
-
-  # Add HEAD URL for cloning the main branch
   head "https://github.com/mpartel/bindfs.git", branch: "main"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
 
   conflicts_with "bindfs", because: "it installs the same binaries"
 
-  # We need pkg-config to find fuse-t
-  depends_on "pkg-config" => :build
-  # Add build dependencies for running ./autogen.sh
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  # depends_on "libtool" => :build
-
-  # As per the README, fuse-t is the dependency for macOS
-  # depends_on "fuse-t"
-
   def install
-    # We need to run ./autogen.sh when building from git
     ENV["PKG_CONFIG_LIBDIR"] = "/usr/local/lib/pkgconfig"
-    # ENV.append "LDFLAGS", "-L/usr/local/lib/test"
     system "./autogen.sh"
     system "./configure", *std_configure_args
     system "make"
@@ -33,8 +22,6 @@ class BindfsFuseT < Formula
   end
 
   test do
-    # Check if the binary runs and reports its version
-    system "#{bin}/bindfs", "--version"
+    assert_match version.to_s, shell_output("#{bin}/bindfs --version")
   end
 end
-
