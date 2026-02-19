9d2aaf3dbbf8382e5dc0c0da444427e7d6a68e9b
Tim Hörmann <pypi@audivir.de>
2026-02-19 23:07:12 +0100

    updated dirdiff

--- Formula/dirdiff.rb
+++ Formula/dirdiff.rb
@@ -1,20 +1,39 @@
 class Dirdiff < Formula
-    desc "Compare two directories recursively"
-    homepage "https://github.com/audivir/dirdiff"
-    url "https://github.com/audivir/dirdiff.git",
-        tag:      "v0.1.0",
-        revision: "2bd1ea40f99a0eb0ce69e1a789ac14bab13550bb"
-    license "MIT"
+  desc "Recursively compare two local or ssh-remote directories"
+  homepage "https://github.com/audivir/dirdiff"
+  version "0.1.2"
 
-    depends_on "go" => :build
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-arm64"
+      sha256 "fd49a3f2a8d68d40da745afae8c38d17064bef5d86b017f662c4b868255581f9"
+    else
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-amd64"
+      sha256 "df0e094c61bfef0709beed4e248079628da331d2d1c25649f24005aabe339f57"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-arm64"
+      sha256 "e89a9532287ff1ec7636c718825d1c1afcc550c7bac0f428a79b56364d8701aa"
+    else
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-amd64"
+      sha256 "1baeb0f71c2f3a3897a9fc2fd82954b12c67a1720f89b08f9d91397d416392cd"
+    end
+  end
 
-    def install
-        system "go", "build", "-o", "dirdiff", "github.com/audivir/dirdiff"
-        bin.install "dirdiff"
+  def install
+    # Rename to just 'dirdiff'
+    binary_name = if OS.mac?
+      Hardware::CPU.arm? ? "dirdiff-darwin-arm64" : "dirdiff-darwin-amd64"
+    else
+      Hardware::CPU.arm? ? "dirdiff-linux-arm64" : "dirdiff-linux-amd64"
     end
 
-    test do
-        expected="Error: usage: dirdiff [options] <dirA> <dirB>"
-        assert_match expected, shell_output(bin/"dirdiff", 2)
-    end
+    bin.install binary_name => "dirdiff"
+  end
+
+  test do
+    # Updated to match our latest usage text
+    assert_match "dirdiff version 0.1.2", shell_output("#{bin}/dirdiff --version")
+  end
 end
