f1a22009d954cfad7a2752843f94e2186b850ea6
Tim Hörmann <t.hoermann@audivir.de>
2026-03-16 02:10:41 +0100

    Update brew

--- Formula/dfhash.rb
+++ Formula/dfhash.rb
@@ -1,23 +1,23 @@
 class Dfhash < Formula
   desc "Deterministic content hasher for tabular data files"
   homepage "https://github.com/audivir/dfhash"
-  version "0.1.2"
+  version "0.1.4"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/dfhash/releases/download/v#{version}/dfhash-darwin-arm64"
-      sha256 "aead7809102bfec497e24c61623f8aeef83e862e31b8f060bd6710eec3dff995"
+      sha256 "4b6646675970b10499755bbed4ea7c5d159187d52f3c81619335e5b3826bc3ea"
     else
       url "https://github.com/audivir/dfhash/releases/download/v#{version}/dfhash-darwin-amd64"
-      sha256 "47e07bba75fb430d6928090ca3de847205bd96b58dda6c1cdf52fc2fb0ece45b"
+      sha256 "e0fe2d74d2e706e14626e88c0cfe4978462b234383cd15a662dee2d6392b9c62"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/dfhash/releases/download/v#{version}/dfhash-linux-arm64"
-      sha256 "c6c44509dda850caa335c67b9cc05392a15189b1c3649980a142d9c6fd7efde1"
+      sha256 "b70b2bca5d9b98e05572f23fe21fe30afc038a2c616ed319c586a15b3d27ef0b"
     else
       url "https://github.com/audivir/dfhash/releases/download/v#{version}/dfhash-linux-amd64"
-      sha256 "06b64831ec14e5590c7ffe5fa0d38efd5da3025200e983fed0868b6384c03394"
+      sha256 "9abba4b4a82cce109b957f39bd556e5dee61f9ba2b51ae6e86ee8c0de6ee1179"
     end
   end
 
