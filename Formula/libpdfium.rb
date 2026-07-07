699f3770a270334995bd77a72ff032a0984eddbb
Tim Hörmann <t.hoermann@audivir.de>
2026-07-07 12:32:04 +0200

    Update brew

--- Formula/libpdfium.rb
+++ Formula/libpdfium.rb
@@ -1,24 +1,24 @@
 class Libpdfium < Formula
   desc "Binary distribution of PDFium (without V8)"
   homepage "https://github.com/bblanchon/pdfium-binaries"
-  version "7906"
+  version "7934"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-mac-arm64.tgz"
-      sha256 "d7179c5667c6ff5032a4e57840df642e094c101f464ec18aabb0ea22fb722f8e"
+      sha256 "24815e8986a85e1ce5553027fc2e038c21d61310665ffdf849625994cc43819c"
     else
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-mac-x64.tgz"
-      sha256 "e15aefb3ed4e86e9bd0b33701b02b6e77c9a6ed51cc53a51f1dd9f5d6a0bd82a"
+      sha256 "7aace004650a5933467c100956bd39feeb36c28d92660dbf0326ad10022f48e9"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-linux-arm64.tgz"
-      sha256 "a188262ab444d43e6030ffabc8b03a7d3a4e3b1146ecbfb030af42f402ee0cff"
+      sha256 "a5ea0edd14b9e352a96f47c9484ebbd24af8dfcadc0f0cb9a199b832d815e75e"
     else
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-linux-x64.tgz"
-      sha256 "e07bc44c4e422c50eb01da742dc1ec59ad6780ce64ed91955533da8e9fe1a363"
+      sha256 "60975aea26c0c6e2f0e0ca3e3c55d184085362f78ac689e4cba2d7d7997920bb"
     end
   end
 
