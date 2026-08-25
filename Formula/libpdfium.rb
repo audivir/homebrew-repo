72bdfa8b4a6975dff04d008ac9f913b77e287b2b
Tim Hörmann <t.hoermann@audivir.de>
2026-08-25 15:06:28 +0200

    Update brew

--- Formula/libpdfium.rb
+++ Formula/libpdfium.rb
@@ -1,24 +1,24 @@
 class Libpdfium < Formula
   desc "Binary distribution of PDFium (without V8)"
   homepage "https://github.com/bblanchon/pdfium-binaries"
-  version "8009"
+  version "8021"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-mac-arm64.tgz"
-      sha256 "b1f2f17c7432a9942514dda5094ee9822c743bdfd07e7187725efbd34fde941f"
+      sha256 "994600fa28974ce09a1c51c35039e808a6bc8ea3839050322c101ab229ad5c96"
     else
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-mac-x64.tgz"
-      sha256 "e0afb6905e97438a9159aaa5b6c4fe188d97a7e3be05dc4782422c3ca7e1ce1a"
+      sha256 "0e770fda56c6726a08fab84c6306ad91eceb10589020ce3a407fad3ebcbe7bb2"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-linux-arm64.tgz"
-      sha256 "ed70360807d9e6bc4ae2b0469b576177ee9e680e73ad8216230bf8c9a6719b9c"
+      sha256 "8da7615b210986a5b7187e3246ef0c315995583c68af6765b070d5dbb156ede6"
     else
       url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/#{version}/pdfium-linux-x64.tgz"
-      sha256 "be513e8021a5bf8eb2116e00d78c3bacb82c5a02b3785156ae14fe5e33084385"
+      sha256 "685f7930cd184ea22cd77afe707c1cf53b173d18118b6e16cb213c9277d7cdc3"
     end
   end
 
--- Formula/mkpasswd-go.rb
+++ Formula/mkpasswd-go.rb
@@ -1,27 +1,39 @@
 class MkpasswdGo < Formula
-    desc "Go-clone of whois' mkpasswd utility"
-    homepage "https://github.com/audivir/mkpasswd-go"
-    url "https://github.com/audivir/mkpasswd-go.git",
-        tag:      "v1.0.0",
-        revision: "098c0653602768813f8fba594d59893a35fbbc5a"
-    license "MIT"
+  desc "Go-clone of whois' mkpasswd utility"
+  homepage "https://github.com/audivir/mkpasswd-go"
+  version "0.1.6"
 
-    depends_on "go" => :build
-
-    on_macos do
-        depends_on "libxcrypt"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-arm64"
+      sha256 "0eb59b84d9d47759992965f537fa0628ec5e566914ec5fd1080eaaa0a9ac6118"
+    else
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-amd64"
+      sha256 "dbba33ff0ab625c0feb6bef6cc2bb3eac947db2b20053b457d46749f6a68e35c"
     end
-
-    def install
-        if OS.mac?
-            ENV.append "CPATH", Formula["libxcrypt"].opt_include
-            ENV.append "LIBRARY_PATH", Formula["libxcrypt"].opt_lib
-        end
-        system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", "mkpasswd-go"
-        bin.install "mkpasswd-go"
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-arm64"
+      sha256 "1bd1a16e02e04ae19a3bbd14de71da46df441cd4f9dd85ce0ff1f299e5e98434"
+    else
+      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-amd64"
+      sha256 "ab50d01a1bd619955c6c94d7aa94dcc04c4993a54625f2821cd68d2ec472bd96"
     end
+  end
 
-    test do
-        assert_match "mkpasswd version mkpasswd-go 1.0", shell_output("#{bin}/mkpasswd-go -v")
+  def install
+    # Rename to just 'mkpasswd-go'
+    binary_name = if OS.mac?
+      Hardware::CPU.arm? ? "dirdiff-darwin-arm64" : "dirdiff-darwin-amd64"
+    else
+      Hardware::CPU.arm? ? "dirdiff-linux-arm64" : "dirdiff-linux-amd64"
     end
+  
+    bin.install binary_name => "mkpasswd-go"
+  end
+
+  test do
+    # Updated to match our latest usage text
+    assert_match "dirdiff version #{version}", shell_output("#{bin}/dirdiff --version")
+  end
 end
