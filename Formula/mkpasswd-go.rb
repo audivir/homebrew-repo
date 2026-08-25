9a4206df40fb763e9515028080928ef390e97ecd
Tim Hörmann <t.hoermann@audivir.de>
2026-08-25 15:11:56 +0200

    Update brew

--- Formula/mkpasswd-go.rb
+++ Formula/mkpasswd-go.rb
@@ -1,20 +1,20 @@
 class MkpasswdGo < Formula
   desc "Go-clone of whois' mkpasswd utility"
   homepage "https://github.com/audivir/mkpasswd-go"
-  version "0.1.6"
+  version "1.0.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-darwin-arm64"
-      sha256 "0eb59b84d9d47759992965f537fa0628ec5e566914ec5fd1080eaaa0a9ac6118"
+      sha256 "e2ce57cb7d47a26a8716aa416e5505bfa987705e1de6a4ef8364b2eae1a59bbf"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-linux-arm64"
-      sha256 "1bd1a16e02e04ae19a3bbd14de71da46df441cd4f9dd85ce0ff1f299e5e98434"
+      sha256 "ff3a37c8b0bf2cc42da9261291cb78a8d326de92f2951a4340166a3114c59cb4"
     else
       url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-linux-amd64"
-      sha256 "ab50d01a1bd619955c6c94d7aa94dcc04c4993a54625f2821cd68d2ec472bd96"
+      sha256 "e80a42f99b3b0501d82fb98a7bb0c3cf3c912d6873e1b787f009e0cbb2374796"
     end
   end
 
