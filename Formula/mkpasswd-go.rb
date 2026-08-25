788851fef38f3d872e7b9ef8cb43d96b0823ad1d
Tim Hörmann <t.hoermann@audivir.de>
2026-08-25 15:10:55 +0200

    Update brew

--- Formula/mkpasswd-go.rb
+++ Formula/mkpasswd-go.rb
@@ -5,18 +5,15 @@ class MkpasswdGo < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-arm64"
+      url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-darwin-arm64"
       sha256 "0eb59b84d9d47759992965f537fa0628ec5e566914ec5fd1080eaaa0a9ac6118"
-    else
-      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-darwin-amd64"
-      sha256 "dbba33ff0ab625c0feb6bef6cc2bb3eac947db2b20053b457d46749f6a68e35c"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-arm64"
+      url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-linux-arm64"
       sha256 "1bd1a16e02e04ae19a3bbd14de71da46df441cd4f9dd85ce0ff1f299e5e98434"
     else
-      url "https://github.com/audivir/dirdiff/releases/download/v#{version}/dirdiff-linux-amd64"
+      url "https://github.com/audivir/mkpasswd-go/releases/download/v#{version}/mkpasswd-go-linux-amd64"
       sha256 "ab50d01a1bd619955c6c94d7aa94dcc04c4993a54625f2821cd68d2ec472bd96"
     end
   end
@@ -24,9 +21,9 @@ class MkpasswdGo < Formula
   def install
     # Rename to just 'mkpasswd-go'
     binary_name = if OS.mac?
-      Hardware::CPU.arm? ? "dirdiff-darwin-arm64" : "dirdiff-darwin-amd64"
+      "mkpasswd-go-darwin-arm64"
     else
-      Hardware::CPU.arm? ? "dirdiff-linux-arm64" : "dirdiff-linux-amd64"
+      Hardware::CPU.arm? ? "mkpasswd-go-linux-arm64" : "mkpasswd-go-linux-amd64"
     end
   
     bin.install binary_name => "mkpasswd-go"
