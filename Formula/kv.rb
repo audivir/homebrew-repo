97da5d856540b2957d709cd1e1292bbe667b1d35
Tim Hörmann <pypi@audivir.de>
2026-09-17 10:36:41 +0200

    Update brew

--- Formula/kv.rb
+++ Formula/kv.rb
@@ -1,7 +1,7 @@
 class Kv < Formula
   desc "Image viewer for the Kitty Terminal Graphics Protocol"
   homepage "https://github.com/audivir/kv"
-  version "0.2.8"
+  version "0.3.0"
 
   depends_on "libpdfium"
   depends_on "libheif"
@@ -9,15 +9,15 @@ class Kv < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/kv/releases/download/v#{version}/kv-aarch64-apple-darwin"
-      sha256 "df7befb91eecbe5c825dda30fb0f2d981a2f6452b4da162043f60b2e542e8518"
+      sha256 "d700aa1abe589c4882ed5c74920dec384d619949a0562d27d74e629cde621da8"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/audivir/kv/releases/download/v#{version}/kv-aarch64-unknown-linux-gnu"
-      sha256 "5440ed33d0e2901043b82a1426db3e14a607262106c8d994972975632388ea1e"
+      sha256 "b69e3cda4535f756d2caec7d1eb6d99945cf7c0eea3542d2d3f78f1f07d4c590"
     else
       url "https://github.com/audivir/kv/releases/download/v#{version}/kv-x86_64-unknown-linux-gnu"
-      sha256 "8ff1b22bf0670817221b3c6c63b339edb4ca067f6d965ce1e0605a192293806f"
+      sha256 "48fdc62287d6c5f9307853d1b2dee2263799fdfeaf6bac4e0bff953c75db202f"
     end
   end
 
